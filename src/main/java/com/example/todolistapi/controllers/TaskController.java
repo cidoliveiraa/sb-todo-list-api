bd9a9336bff7772ad527a7e26be6697f55409646
Aparecido Muniz <cidoliveiraa@gmail.com>
2019-10-22 13:14:53 -0300

    Criando endpoints CRUD

--- src/main/java/com/example/todolistapi/controllers/TaskController.java
+++ src/main/java/com/example/todolistapi/controllers/TaskController.java
@@ -1,9 +1,13 @@
 package com.example.todolistapi.controllers;
 
+import java.time.LocalDateTime;
+
+import com.example.todolistapi.models.StatusTask;
 import com.example.todolistapi.models.Task;
-import com.example.todolistapi.repository.TaskRepository;
+import com.example.todolistapi.repositories.TaskRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +21,12 @@ public class TaskController {
 
   @RequestMapping(method = RequestMethod.POST, value = "/tasks")
   public String create(@RequestBody Task task) {
+    if (task.getStatus() == null) {
+      task.setStatus(StatusTask.PENDING);
+    }
+    if (task.getDate() == null) {
+      task.setDate(LocalDateTime.now());
+    }
     taskRepository.save(task);
     return task.getId();
   }
@@ -24,5 +34,30 @@ public class TaskController {
   @RequestMapping(method = RequestMethod.GET, value = "/tasks")
   public Iterable<Task> list() {
     return taskRepository.findAll();
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/tasks/{id}")
+  public Task show(@PathVariable String id) {
+    return taskRepository.findById(id).get();
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, value = "/tasks/{id}")
+  public Task update(@PathVariable String id, @RequestBody Task task) {
+    Task taskToUpd = taskRepository.findById(id).get();
+    if (task.getName() != null) {
+      taskToUpd.setName(task.getName());
+    }
+    if (task.getStatus() != null) {
+      taskToUpd.setStatus(task.getStatus());
+    }
+    taskRepository.save(taskToUpd);
+    return taskToUpd;
+  }
+
+  @RequestMapping(method = RequestMethod.DELETE, value = "/tasks/{id}")
+  public String delete(@PathVariable String id) {
+    Task task = taskRepository.findById(id).get();
+    taskRepository.delete(task);
+    return "Product Deleted";
   }
 }
--- src/main/java/com/example/todolistapi/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todolistapi.repository;
-
-import com.example.todolistapi.models.Task;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task,String>{
-  
-} 
